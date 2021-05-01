273de106b3e73bf8c2da5ec0c9bd590036ef1933
Sunanda <sunandabag63@gmail.com>
2021-05-01 13:53:52 +0530

    removed var/www/static from settings file

--- Ecommerce/Ecommerce/settings.py
+++ Ecommerce/Ecommerce/settings.py
@@ -126,7 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    '/var/www/static/',
+    
 ]
 
 MEDIA_URL = '/images/'
